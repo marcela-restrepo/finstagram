e4331ba37f87de5045350fa95418bbca099a40c3
Marcela Restrepo <marcerestrepo96@gmail.com>
2023-04-26 23:44:38 +0000

    W2D2

---
+++ demo.rb
@@ -0,0 +1,24 @@
+# DATA TYPES
+# string
+
+puts 'hello here'
+puts "Hello there"
+
+# numbers
+
+puts 100
+puts 100.34
+
+puts 35-8
+
+# booleans
+puts 10 == 100 
+
+# variables
+
+full_name = 'Marcela Restrepo'
+age = 100
+is_red = false
+
+puts "hello my name is #{full_name}" and I'm #(age) years old and I do like red: #(is_red)"
+
